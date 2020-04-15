5fa0300a054872fa855a3c687d9df8103f38c802
akiiita <xhw@akiiita.com>
2020-04-16 01:47:14 +0800

    修改Analyse传入音乐平台规则

--- Music/api_views.py
+++ Music/api_views.py
@@ -1,20 +1,25 @@
 from django.views import View
 from musicdl import musicdl
-from SmartDjango import Analyse, P, PList
+from SmartDjango import Analyse, P, PList, E
 # from smartify import PList
-
+def is_platform(app):
+    # for app in args:
+        if app not in ['baiduFlac', 'kugou', 'kuwo', 'qq', 'netease', 'migu', 'xiami', 'joox']:
+            raise E("传入音乐平台错误")
 
 class MusicView(View):
     @staticmethod
     @Analyse.r(b=[
-        PList(name='targetsrcs', read_name='xxx').set_child(
-            P('targetsrcs', '音乐平台').process(lambda x: x in ['qq', 'netease', 'baidu'])
+        PList(name='targetsrcs', read_name='音乐平台').set_child(
+            P('targetsrc', '音乐平台').validate(lambda x: is_platform(x))
+            # P('targetsrc', '音乐平台').validate(lambda x: x in ['baiduFlac', 'kugou', 'kuwo', 'qq', 'netease', 'migu', 'xiami', 'joox'])
         ),
         # P('targetsrcs', '音乐平台').process(list),
         P('musicname', '名字').process(str),
 
     ])
     def post(request):
+        print(request.d.musicname)
         print(request.d.targetsrcs)
         config = {'logfilepath': 'musicdl.log', 'savedir': 'downloaded', 'search_size_per_source': 5, 'proxies': {}}
         target_srcs = request.d.targetsrcs
