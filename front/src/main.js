43b1f157483dca27dd5324143fdff2278593cb65
akiiita <xhw@akiiita.com>
2020-04-17 16:13:02 +0800

    debug element

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "./assets/css/base.css";
+import "./plugins/element.js";
+import router from "./router";
 
+axios.interceptors.request.use(function(config) {
+  config.headers.token = window.sessionStorage.getItem("token");
+  return config;
+});
 Vue.config.productionTip = false;
-
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = "/api";
 new Vue({
+  render: h => h(App),
   router,
-  store,
-  render: h => h(App)
+  store
 }).$mount("#app");
